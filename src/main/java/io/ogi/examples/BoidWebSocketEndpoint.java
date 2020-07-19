5bae049506ba1a708e0cfe5dca768b1cbf24dfd1
Petra Nemeth <nemeth.petra.xy@gmail.com>
2020-07-19 11:37:26 +0200

    boids are flying, process can be restarted from frontend

--- src/main/java/io/ogi/examples/BoidWebSocketEndpoint.java
+++ src/main/java/io/ogi/examples/BoidWebSocketEndpoint.java
@@ -4,9 +4,7 @@ import io.helidon.common.configurable.ScheduledThreadPoolSupplier;
 import io.helidon.common.configurable.ThreadPoolSupplier;
 
 import javax.websocket.*;
-import java.io.IOException;
 import java.util.concurrent.*;
-import java.util.logging.Level;
 import java.util.logging.Logger;
 
 public class BoidWebSocketEndpoint extends Endpoint {
@@ -17,68 +15,48 @@ public class BoidWebSocketEndpoint extends Endpoint {
     private static ScheduledExecutorService scheduledExecutorService;
     private static ExecutorService executor;
 
-    private final MessageQueue messageQueue = MessageQueue.instance();
     private final BoidSimulation boidSimulation;
-    private MessageQueueTaker messageQueueTaker;
-    //    private int counter;
 
     public BoidWebSocketEndpoint(BoidSimulation boidSimulation) {
         this.boidSimulation = boidSimulation;
-//        counter = 0;
     }
 
     @Override
     public void onOpen(Session session, EndpointConfig endpointConfig) {
         LOGGER.info("Opening session " + session.getId());
-        messageQueueTaker = new MessageQueueTaker(session);
+        MessageQueueTaker messageQueueTaker = new MessageQueueTaker(session);
         session.addMessageHandler(new MessageHandler.Whole<String>() {
             @Override
             public void onMessage(String message) {
                 if (message.equalsIgnoreCase(START)) {
-                    System.out.println("here");
+                    LOGGER.info("start message");
                     scheduledExecutorService = ScheduledThreadPoolSupplier.builder()
                             .threadNamePrefix("boid-simulation-thread")
                             .corePoolSize(1)
                             .daemon(false)
                             .build()
                             .get();
-                    executor = ThreadPoolSupplier.builder()
-                            .threadNamePrefix("boid-message-queue-taker-thread")
-                            .corePoolSize(1)
-                            .daemon(true)
-                            .build()
-                            .get();
 
                     boidSimulation.initializeBoids();
-                    executor.execute(messageQueueTaker);
                     scheduledExecutorService.scheduleAtFixedRate(boidSimulation, 5,boidSimulation.getBoidModel().simulationSpeed, TimeUnit.MILLISECONDS);
                 }
                 if (message.equalsIgnoreCase(STOP)) {
-                    System.out.println("there");
-                    executor.shutdownNow();
+                    LOGGER.info("stop message");
                     scheduledExecutorService.shutdownNow();
                 }
                 System.out.println("Received message:"+ message + ":");
             }
         });
 
-//        counter = 0;
-//        scheduledExecutorService = ScheduledThreadPoolSupplier.builder()
-//                .threadNamePrefix("boid-simulation-thread")
-//                .corePoolSize(1)
-//                .daemon(false)
-//                .build()
-//                .get();
-//        executor = ThreadPoolSupplier.builder()
-//                .threadNamePrefix("boid-message-queue-taker-thread")
-//                .corePoolSize(1)
-//                .daemon(true)
-//                .build()
-//                .get();
-//
-//        boidSimulation.initializeBoids();
-//        executor.execute(messageQueueTaker);
-//        scheduledExecutorService.scheduleAtFixedRate(boidSimulation, 5,boidSimulation.getBoidModel().simulationSpeed, TimeUnit.MILLISECONDS);
+        executor = ThreadPoolSupplier.builder()
+                .threadNamePrefix("boid-message-queue-taker-thread")
+                .corePoolSize(1)
+                .daemon(true)
+                .build()
+                .get();
+
+        boidSimulation.initializeBoids();
+        executor.execute(messageQueueTaker);
     }
 
     @Override
@@ -86,13 +64,7 @@ public class BoidWebSocketEndpoint extends Endpoint {
         super.onClose(session, closeReason);
 
         LOGGER.info("Closing session " + session.getId());
-        if (!executor.isShutdown()) {
-            executor.shutdown();
-        }
-        if (!scheduledExecutorService.isShutdown()) {
-            scheduledExecutorService.shutdown();
-        }
+        executor.shutdownNow();
+        scheduledExecutorService.shutdownNow();
     }
-
-
 }
